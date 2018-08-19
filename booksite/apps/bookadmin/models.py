1adb746d457d2ab0193dd814f3c5a0461f4dfc65
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2018-08-19 17:27:47 -0400

    Added the custom model

--- booksite/apps/bookadmin/models.py
+++ booksite/apps/bookadmin/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=50)
-    publication_date = models.DateField(null=True)
-    author = models.CharField(max_length=30, blank=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    number_of_pages = models.IntegerField(blank=True, null=True)
+	title = models.CharField(max_length=50)
+	publisher = models.CharField(max_length=50)
+	author = models.CharField(max_length=30, blank=True)
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+	pages = models.IntegerField(blank=True, null=True)
